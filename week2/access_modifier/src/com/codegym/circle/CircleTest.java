529804d98e7658ec5ed83d15f75275a198c12142
Khiên Nguyễn <thanhkhien1994@gmail.com>
2020-10-20 20:04:14 +0700

    add file bt

---
+++ week2/access_modifier/src/com/codegym/circle/CircleTest.java
@@ -0,0 +1,11 @@
+package com.codegym.circle;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println("Diện tích là: "+circle.getArea());
+        System.out.println("Bán kính là: "+circle.getRadius());
+
+
+    }
+}
